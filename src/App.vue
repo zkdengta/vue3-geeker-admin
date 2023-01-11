1d434c0858db50f21e6aee35d53eddd58a0b264f
zkdengta <1015422160@qq.com>
2023-01-11 11:47:20 +0800

    登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,12 @@
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-import {Search, Edit,Check,Message,Star, Delete} from '@element-plus/icons-vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <div style="text-align: center;margin-top: 10px">
-    <el-button :icon="Search" circle></el-button>
-    <el-button type="primary" :icon="Edit" circle></el-button>
-    <el-button type="success" :icon="Check" circle></el-button>
-    <el-button type="info" :icon="Message" circle></el-button>
-    <el-button type="warning" :icon="Star" circle></el-button>
-    <el-button type="danger" :icon="Delete" circle></el-button>
-  </div>
+  <ElConfigProvider>
+		<RouterView></RouterView>
+	</ElConfigProvider>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+<script setup lang="ts">
+
+
+</script>
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+// reset style sheet
+import "@/styles/reset.scss";
 //全局注册Element-Plus组件
 // element plus
 import ElementPlus from "element-plus";
@@ -10,6 +11,19 @@ import * as Icons from "@element-plus/icons-vue";
 import "element-plus/dist/index.css";
 // element dark(内置暗黑模式)
 import "element-plus/theme-chalk/dark/css-vars.css";
+// // custom element dark(自定义暗黑模式)
+// import "@/styles/theme/element-dark.scss";
+// // custom element css
+// import "@/styles/element.scss";
+// vue Router
+import router from '@/routers/index';
+// // vue i18n
+// import I18n from "@/languages/index";
+// // pinia store
+// import pinia from "@/stores/index";
+
+
+
 
 const app =  createApp(App)
 
@@ -18,4 +32,4 @@ Object.keys(Icons).forEach(key =>{
     app.component(key,Icons[key as keyof typeof Icons])
 })
 
-app.use(ElementPlus).mount('#app')
+app.use(ElementPlus).use(router).mount('#app')
